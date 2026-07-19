9d2883c7f0db4d7736ddbf5010e36e58829c0e14
PG408 <peter.gao418@hotmial.com>
2026-07-19 17:35:24 +0800

    🔧 chore(release): bump app version to 0.1.6

--- build.sh
+++ build.sh
@@ -30,8 +30,8 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key><string>gaobo.gl.codex-status-bar</string>
   <key>CFBundleExecutable</key><string>CodexStatusBar</string>
   <key>CFBundleIconFile</key><string>AppIcon</string>
-  <key>CFBundleVersion</key><string>0.1.5</string>
-  <key>CFBundleShortVersionString</key><string>0.1.5</string>
+  <key>CFBundleVersion</key><string>0.1.6</string>
+  <key>CFBundleShortVersionString</key><string>0.1.6</string>
   <key>CFBundlePackageType</key><string>APPL</string>
   <key>LSMinimumSystemVersion</key><string>12.0</string>
   <key>LSUIElement</key><true/>
